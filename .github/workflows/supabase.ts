fa7102a807b1b2c07f8b73fb127630a040566859
tomo-x7 <158121497+tomo-x7@users.noreply.github.com>
2024-04-03 00:10:21 +0900

    復号処理部分にエラーハンドリングを追加

--- .github/workflows/supabase.ts
+++ .github/workflows/supabase.ts
@@ -10,43 +10,39 @@ export type UserData = {
 	github_name: string;
 	fail_count: number;
 	iv: string;
-	DID:string
+	DID: string;
 };
 let supabase: SupabaseClient | undefined;
-export const supabasesetting = (
-	supabase_url: string,
-	supabase_token: string,
-) => {
+export const supabasesetting = (supabase_url: string, supabase_token: string) => {
 	supabase = createClient(supabase_url, supabase_token);
 };
 
 export const getUsersList = async (): Promise<Array<UserData>> => {
-	if (supabase === undefined)
-		throw new Error("Please run supabase settings before");
+	if (supabase === undefined) throw new Error("Please run supabase settings before");
 	const ret: Array<UserData> = [];
 	const rawdata: Array<UserData> = await supabase
 		.from("userdata")
 		.select()
 		.then((data) => {
-			if (
-				/2\d{2}/.test(data.status.toString()) &&
-				data.data &&
-				data.data.length !== 0
-			) {
+			if (/2\d{2}/.test(data.status.toString()) && data.data && data.data.length !== 0) {
 				return data.data;
 			}
 			throw new Error("エラー");
 		});
 	for (const i in rawdata) {
-		rawdata[i].bsky_password=decrypt(rawdata[i].bsky_password,rawdata[i].iv)
-		ret.push(rawdata[i]);
+		try {
+			rawdata[i].bsky_password = decrypt(rawdata[i].bsky_password, rawdata[i].iv);
+			ret.push(rawdata[i]);
+		} catch (e) {
+			writelog(`${rawdata[i].DID}'s password or iv has error`);
+			fail(rawdata[i].id, rawdata[i].fail_count);
+		}
 	}
 	return ret;
 };
 
 export const deleteuser = async (id: number) => {
-	if (supabase === undefined)
-		throw new Error("Please run supabase settings before");
+	if (supabase === undefined) throw new Error("Please run supabase settings before");
 	supabase
 		.from("userdata")
 		.delete()
@@ -58,8 +54,7 @@ export const deleteuser = async (id: number) => {
 		});
 };
 export const success = async (id: number) => {
-	if (supabase === undefined)
-		throw new Error("Please run supabase settings before");
+	if (supabase === undefined) throw new Error("Please run supabase settings before");
 	supabase
 		.from("userdata")
 		.update({ fail_count: 0 })
@@ -72,8 +67,7 @@ export const success = async (id: number) => {
 		});
 };
 export const fail = async (id: number, fail_count: number) => {
-	if (supabase === undefined)
-		throw new Error("Please run supabase settings before");
+	if (supabase === undefined) throw new Error("Please run supabase settings before");
 	supabase
 		.from("userdata")
 		.update({ fail_count: fail_count + 1 })
@@ -87,8 +81,7 @@ export const fail = async (id: number, fail_count: number) => {
 };
 
 export const writelog = (log: string | number | Error | unknown) => {
-	if (supabase === undefined)
-		throw new Error("Please run supabase settings before");
+	if (supabase === undefined) throw new Error("Please run supabase settings before");
 	supabase
 		.from("errorlog")
 		.insert({ errorlog: log })
