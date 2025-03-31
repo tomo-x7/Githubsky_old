de72820aebf230f4e2a54bd1158ac0384c8e07be
tomo_x <158121497+tomo-x7@users.noreply.github.com>
2025-03-31 17:35:02 +0900

    search console

--- webpage/app/layout.tsx
+++ webpage/app/layout.tsx
@@ -7,6 +7,7 @@ export default function RootLayout({
 	return (
 		<html lang="ja">
 			<head>
+			<meta name="google-site-verification" content="YixWpicRcAoWs75i4q6tbR_KwaSRuVetnSE4_fllXvo" />
 			</head>
 			<body>{children}</body>
 		</html>
